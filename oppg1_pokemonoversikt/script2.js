97c276152397c776cb517cba0636a8a625f0d1e8
megahwatt <156299328+megahwatt@users.noreply.github.com>
2024-04-04 11:53:35 +0200

    pokeoversikt-fetching from several urls

--- oppg1_pokemonoversikt/script2.js
+++ oppg1_pokemonoversikt/script2.js
@@ -1,27 +1,27 @@
 // Fetch API and data about each pokémon in Gen I, II, III and IV
-/*
+
 async function gottaCatchEmAll() {
 	try {
-        URLs
+		const [dataGenI, dataGenII, dataGenIII, dataGenIV] = await Promise.all([
+			(await fetch("https://pokeapi.co/api/v2/generation/1")).json(),
+			(await fetch("https://pokeapi.co/api/v2/generation/2")).json(),
+			(await fetch("https://pokeapi.co/api/v2/generation/3")).json(),
+			(await fetch("https://pokeapi.co/api/v2/generation/4")).json(),
+		]);
 
-        const genI await (await fetch("")).json();
-        const genII await (await fetch("")).json();
-        const genIII await (await fetch("")).json();
-        const genIV await (await fetch("")).json();
-        
+		console.log("genI", dataGenI);
+		console.log("genII", dataGenII);
+		console.log("genIII", dataGenIII);
+		console.log("genIV", dataGenIV);
+
+		/*
         const pokeSpecies = caughtEmAll.pokemon_species;
-
-        console.log(genI);
-        console.log(genII);
-        console.log(genIII);
-        console.log(genIV);
 
         const pokeNames = pokeSpecies.map((pokemon) => pokemon.name);
 
 		const generationOne = await (await fetch("https://pokeapi.co/api/v2/generation/1")).json();
 		const pokeSpecies = generationOne.pokemon_species;
-
-		
+*/
 
 		console.log("Gotcha! First four generations were caught!");
 		return pokeNames;
@@ -29,4 +29,4 @@ async function gottaCatchEmAll() {
 		console.error("Oh no, the Pokémons broke free!", error);
 	}
 }
-*/
+gottaCatchEmAll();
