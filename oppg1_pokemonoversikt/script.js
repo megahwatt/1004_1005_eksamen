a62f4da5916b8c55440e1bf2d8c18c6a686bd694
megahwatt <156299328+megahwatt@users.noreply.github.com>
2024-04-08 16:50:59 +0200

    pokedex-edit works on localstorage, savedpokes array and pokearray

--- oppg1_pokemonoversikt/script.js
+++ oppg1_pokemonoversikt/script.js
@@ -324,7 +324,24 @@ function createEditBtn(index) {
 
 	return editBtn;
 }
-/*
+
+function updateShownPokes(index, newPokeName, newPokeType) {
+	applyEdits(index, newPokeName, newPokeType, pokeArray);
+	applyEdits(index, newPokeName, newPokeType, savedPokes);
+}
+
+function applyEdits(index, newPokeName, newPokeType) {
+	if (masterballs && index >= 0 && index < masterballs.length) {
+		const name = masterballs[index].querySelector(".name");
+		const typeName = masterballs[index].querySelector(".type-name");
+
+		name.innerHTML = newPokeName;
+		typeName.innerHTML = typeInfo.find((type) => type.id === newPokeType)?.name || "";
+	} else {
+		console.error("Arrayet er tomt./Finner ikke index.");
+	}
+}
+
 function editPokemon(index) {
 	const newPokeName = prompt("Gi Pokémonen et kallenavn!");
 	const newPokeType = parseInt(prompt("Skriv inn et tall fra 1-18 for å endre Pokémonen's type."));
@@ -332,32 +349,20 @@ function editPokemon(index) {
 	if (newPokeName && newPokeType >= 1 && newPokeType <= 18) {
 		if (index >= 0 && index < pokeArray.length) {
 			pokeArray[index].name = newPokeName;
-
 			pokeArray[index].pokeTypeID = newPokeType;
-
 			pokeArray[index].type = typeInfo.find((type) => type.id === newPokeType).name;
-
 			localStorage.setItem("pokeArray", JSON.stringify(pokeArray));
+			updateShownPokes(index, newPokeName, newPokeType);
 		} else if (index >= 0 && index < savedPokes.length) {
 			savedPokes[index].name = newPokeName;
-
 			savedPokes[index].pokeTypeID = newPokeType;
-
 			savedPokes[index].type = typeInfo.find((type) => type.id === newPokeType).name;
-
 			localStorage.setItem("savedPokes", JSON.stringify(savedPokes));
+			updateShownPokes(index, newPokeName, newPokeType);
 		} else {
-			console.error("Finner ikke index.");
+			console.error("Arrayet er tomt./Finner ikke index.");
 		}
 	} else {
 		alert("Ugyldig input. Vennligst velg et tall fra 1 til 18.");
 	}
-	updateSavedPokemons();
-	updateShownPokes(index, newPokeName, newPokeType);
 }
-
-function updateShownPokes(index, newPokeName, newPokeType) {
-	masterballs[index].querySelector(".name").innerHTML = newPokeName;
-	masterballs[index].querySelector(".type-name").innerHTML = typeInfo.find((type) => type.id === newPokeType).name;
-}
-*/
